4b74b7193d512d47c55197912c0de80d47ce74a2
Yeliz Tan <yeliztan2905@gmail.com>
2024-09-22 01:49:53 +0200

    v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,8 @@ public class Runner {
 
         System.out.println("versiyon 1");
         System.out.println("versiyon 2");
-        System.out.println("versiyon 3");
+        System.out.println("versiyon 2 nin devami");
+        System.out.println("versiyon 4");
 
     }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,6 @@
+public class User {
+    public static void main(String[] args) {
+
+        System.out.println("versiyon 3ggit ");
+    }
+}
