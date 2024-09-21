9c9074cf78b8acc3820615750fecfaf9e9f0409a
Yeliz Tan <yeliztan2905@gmail.com>
2024-09-22 01:32:01 +0200

    V2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("versiyon 1");
+        System.out.println("versiyon 2");
+        System.out.println("versiyon 3");
 
     }
 }
